9c5017e88dfd098c9f2d66596d86e1ff10d5a0f5
xpdu <teterevkov.ivan@gmail.com>
2016-10-30 19:01:45 +0000

    upd: assign player UUID and name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_player
+
+  def set_player
+    if not session.key?(:player_uuid)
+      session[:player_uuid] = SecureRandom.uuid
+    end
+
+    if not session.key?(:player_name)
+      session[:player_name] = [:Oliver, :Jack, :Harry, :Jacob, :Charlie].sample
+    end
+  end
 end
