8c5e6327e6597171644077b89d4559610e629e80
xpdu <teterevkov.ivan@gmail.com>
2016-12-11 19:00:35 +0000

    impl: badge_partial and button_partial

--- app/helpers/fleet_helper.rb
+++ app/helpers/fleet_helper.rb
@@ -1,9 +1,12 @@
 module FleetHelper
-  def badge_partial
-    "badge"
+  def badge_partial(fleet)
+    return "badge_you" if fleet.owner_uuid == session[:player_uuid]
+    return "badge_approved" if fleet.owner_uuid != @battle.creator_uuid and fleet.is_approved
   end
 
-  def button_partial
-    "button"
+  def button_partial(fleet)
+    return nil if @battle.fleets.any? { |f| f.owner_uuid != @battle.creator_uuid and f.is_approved }
+    return nil unless @battle.creator_uuid == session[:player_uuid]
+    return "button_approve" unless fleet.owner_uuid == @battle.creator_uuid
   end
 end
