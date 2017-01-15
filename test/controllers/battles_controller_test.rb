6650ea8dc53410e41fcf64ea6bb5501263e83924
xpdu <teterevkov.ivan@gmail.com>
2017-01-15 21:11:05 +0000

    add: html assert test

--- test/controllers/battles_controller_test.rb
+++ test/controllers/battles_controller_test.rb
@@ -6,6 +6,7 @@ class BattlesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:blank_battles)
     assert_not_nil assigns(:ongoing_battles)
+    assert_select("#link_to_new_battle")
   end
 
   test "should create battle" do
